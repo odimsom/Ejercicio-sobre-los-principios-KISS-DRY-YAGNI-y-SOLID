0ab2250d89af5b47e8b3a2585651b83aa25d81ec
Francisco Castro <borrome941@gmail.com>
2025-02-17 07:41:26 -0400

    Add project files.

---
+++ LogNotifications.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Principios
+{
+    public class LogNotifications
+    {
+        public void LogNotification(string message)
+        {
+            Console.WriteLine($"login notification: {message}");
+        }
+    }
+}
